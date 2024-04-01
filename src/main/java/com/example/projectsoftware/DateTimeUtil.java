0533c018f8103c649498670cf8c23763ae0669d9
Tasneemjawabrah <160266309+Tasneemjawabrah@users.noreply.github.com>
2024-04-01 21:16:32 +0200

    Update DateTimeUtil.java

--- src/main/java/com/example/projectsoftware/DateTimeUtil.java
+++ src/main/java/com/example/projectsoftware/DateTimeUtil.java
@@ -11,19 +11,20 @@ import java.time.LocalTime;
 
 
 public class DateTimeUtil {
+ private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = getPasswordFromEnvironment();
 
     public static boolean isTimeAvailable(String date, String startTimeStr, String endTimeStr) {
         LocalDate targetDate = LocalDate.parse(date);
         LocalTime startTime = LocalTime.parse(startTimeStr);
         LocalTime endTime = LocalTime.parse(endTimeStr);
-
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "1482003")) {
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD)) {
             String query = "SELECT COUNT(*) FROM software.reservations WHERE date = ? AND starttime < ? AND endtime > ?";
             try (PreparedStatement statement = connection.prepareStatement(query)) {
                 statement.setObject(1, targetDate);
                 statement.setObject(2, endTime);
                 statement.setObject(3, startTime);
-
                 try (ResultSet resultSet = statement.executeQuery()) {
                     if (resultSet.next()) {
                         int count = resultSet.getInt(1);
@@ -34,7 +35,14 @@ public class DateTimeUtil {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return true;
+    }
+
+    private static String getPasswordFromEnvironment() {
+        String password = System.getenv("1482003");
+        if (password == null) {
+            throw new IllegalStateException("Database password not found in environment variables.");
+        }
+        return password;
     }
 }
