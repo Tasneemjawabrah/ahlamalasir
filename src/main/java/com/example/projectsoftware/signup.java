2919d689af518e6d0e36f1f86b3952abd98d63d5
Tasneemjawabrah <160266309+Tasneemjawabrah@users.noreply.github.com>
2024-04-01 22:07:27 +0200

    Update signup.java

--- src/main/java/com/example/projectsoftware/signup.java
+++ src/main/java/com/example/projectsoftware/signup.java
@@ -22,7 +22,7 @@ import java.util.logging.Logger;
 public class signup  implements Initializable {
     private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USERNAME = "postgres";
-
+    private static final String pass =getPasswordFromEnvironment();
 
     static Logger logger = Logger.getLogger(signup.class.getName());
     public TextField id;
@@ -76,7 +76,7 @@ public class signup  implements Initializable {
         String roleValue = checkbox.getValue();
         String code = code1.getText();
 
-        try (Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME,  getPasswordFromEnvironment() )) {
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME,pass )) {
             String sql = "INSERT INTO software.users (userid, firstname, lastname, username, password, email, role,code) VALUES (?,?, ?, ?, ?, ?, ?, ?)";
             try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
                 preparedStatement.setInt(1, idValue);
@@ -147,7 +147,7 @@ public class signup  implements Initializable {
         PreparedStatement statement = null;
         ResultSet resultSet = null;
         try {
-            connection = DriverManager.getConnection(JDBC_URL, USERNAME,  getPasswordFromEnvironment() );
+            connection = DriverManager.getConnection(JDBC_URL, USERNAME, pass);
             String sql = "SELECT COUNT(*) FROM software.users WHERE CAST(userid AS TEXT) = ?";
             statement = connection.prepareStatement(sql);
             statement.setString(1, id);
@@ -186,7 +186,7 @@ public class signup  implements Initializable {
         PreparedStatement statement = null;
         ResultSet resultSet = null;
         try {
-            connection = DriverManager.getConnection(JDBC_URL, USERNAME,  getPasswordFromEnvironment() );
+            connection = DriverManager.getConnection(JDBC_URL, USERNAME,pass );
             String sql = "SELECT COUNT(*) FROM software.users WHERE email = ?";
             statement = connection.prepareStatement(sql);
             statement.setString(1, email);
