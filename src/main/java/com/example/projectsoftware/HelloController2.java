085d3e95951194667c43d1a690e61d2003008ead
Aseelmaradwi <160270378+Aseelmaradwi@users.noreply.github.com>
2024-04-03 00:37:33 +0300

    Update HelloController2.java

--- src/main/java/com/example/projectsoftware/HelloController2.java
+++ src/main/java/com/example/projectsoftware/HelloController2.java
@@ -6,20 +6,15 @@ public class HelloController2 {
     private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String DB_USER = "postgres";
 
-    public boolean login1Clicked(String eemail, String passw) {
+    public boolean login1Clicked(String email, String password) throws DatabaseException {
         String query = "SELECT email, password, role FROM software.users WHERE email = ? AND password = ?";
-
         try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, getPasswordFromEnvironment());
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-
-            preparedStatement.setString(1, eemail);
-            preparedStatement.setString(2, passw);
-
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
             ResultSet rs = preparedStatement.executeQuery();
-
             if (rs.next()) {
                 String role = rs.getString("role");
-
                 switch (role) {
                     case "customer":
                     case "event_planner":
@@ -30,27 +25,22 @@ public class HelloController2 {
                 }
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DatabaseException("Error occurred while logging in", e);
         }
         return false;
     }
 
-    public boolean checkbutton(String email, String code) {
+    public boolean checkButton(String email, String code) {
         if (email.isEmpty() || code.isEmpty()) {
             System.out.println("Email or code is empty.");
             return false;
         }
-
         String query = "SELECT userid FROM software.users WHERE email = ? AND code = ?";
-
         try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, getPasswordFromEnvironment());
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-
             preparedStatement.setString(1, email);
             preparedStatement.setString(2, code);
-
             ResultSet resultSet = preparedStatement.executeQuery();
-
             if (resultSet.next()) {
                 System.out.println("Email and code are correct.");
                 return true;
@@ -58,22 +48,18 @@ public class HelloController2 {
                 System.out.println("Email or code is incorrect.");
                 return false;
             }
-
         } catch (SQLException e) {
             System.err.println("Error while checking availability: " + e.getMessage());
             return false;
         }
     }
 
-    public boolean uservalid(int userId) {
+    public boolean userValid(int userId) {
         String query = "SELECT COUNT(*) FROM software.users WHERE userid = ?";
         boolean userIdValid = false;
-
         try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, getPasswordFromEnvironment());
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-
             preparedStatement.setInt(1, userId);
-
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 if (resultSet.next()) {
                     int count = resultSet.getInt(1);
@@ -81,9 +67,8 @@ public class HelloController2 {
                 }
             }
         } catch (SQLException e) {
-            System.err.println("Error while checking availability: " + e.getMessage());
+            System.err.println("Error while checking user validity: " + e.getMessage());
         }
-
         return userIdValid;
     }
 
